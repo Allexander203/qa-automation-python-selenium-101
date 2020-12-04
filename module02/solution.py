2e3815a918d99eac7e877dc4e0c9bb13f3d27980
Alexander Tsvetanov <vekssist@gmail.com>
2020-12-04 17:37:56 +0200

    Homework

---
+++ module02/solution.py
@@ -0,0 +1,79 @@
+def num_add(a=1,b=0):
+
+	return a+b
+
+def num_sub(a,b):
+
+	return a-b
+
+def num_mul(a,b):
+
+	return a*b
+
+def num_div(a,b):
+
+	return a/b
+
+def num_floor(a,b):
+
+	return a//b
+
+def num_rem(a,b):
+
+	return a%b
+
+IS_TRUE = True 
+IS_FALSE = False 
+print(bool( IS_TRUE))
+
+print(bool( IS_FALSE))
+
+PANCAKE_INGREDIENTS = dict(
+  flour = 2,
+  eggs = 4,
+  milk = 200,
+  butter = False,
+  salt = 0.001)
+
+def ingredient_exists(ingr, dict):
+	return (bool(ingr))
+
+def fatten_pancakes(dict):
+	return PANCAKE_INGREDIENTS(eggs = 6, butter = True)
+
+def add_sugar(dict):
+	return PANCAKE_INGREDIENTS(sugar=0)
+
+def remove_salt(dict):
+	del PANCAKE_INGREDIENTS['salt']
+	return PANCAKE_INGREDIENTS()
+
+lst = []
+lst.append(1)
+lst.append(1)
+lst.append(2)
+lst.append(3)
+lst.append(5)
+lst.append(8)
+lst.append(13)
+lst.append(21)
+lst.append(34)
+lst.append(55)
+lst.append(89)
+lst.append(144)
+
+# print(FIBONACCI_NUMBERS)
+
+def add_fibonacci(lst): 
+	lst.append(len(lst[])-1+len(lst[])-2)
+	return lst
+
+def fib_exists(lst, n):
+	return bool(n in FIBONACCI_NUMBERS)
+# За тази задача не разбрах условието напълно .
+def which_fib(lst, n):
+	return n
+
+
+
+
